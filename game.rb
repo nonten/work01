b4f3a8aeccd405190990241f5113bf763fd6c3ae
Tennon Barnett <Nonten18@gmail.com>
2018-11-16 07:14:05 -0600

    Clean coding up

--- game.rb
+++ game.rb
@@ -1,19 +1,19 @@
-print "Please enter a verb:" + " "
-verb = gets
+print "Please enter a verb: "
+verb = gets.chomp
 
-print "Please enter a noun:" + " "
-noun = gets
+print "Please enter a noun: "
+noun = gets.chomp
 
-print "Please enter a adjective:" + " "
-adjective = gets
+print "Please enter a adjective: "
+adjective = gets.chomp
 
-print "Please enter a second noun:" + " "
-noun2 = gets
+print "Please enter a second noun: "
+noun2 = gets.chomp
 
-puts "One day, I decided to learn to #{verb.chomp} in Ruby."
+puts "One day, I decided to learn to #{verb} in Ruby."
 
-puts "So I turned on my #{noun.chomp} and logged in to Treehouse."
+puts "So I turned on my #{noun} and logged in to Treehouse."
 
-puts "Their teachers were really #{adjective.chomp}."
+puts "Their teachers were really #{adjective}."
 
-puts "In no time, I'd learned to program a simple #{noun2.chomp}!"
+puts "In no time, I'd learned to program a simple #{noun2}!"
