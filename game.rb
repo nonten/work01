129b8eb507f25cc52f42117b77a8cc87b4548f07
Tennon Barnett <Nonten18@gmail.com>
2018-11-16 06:59:54 -0600

    Start up

---
+++ game.rb
@@ -0,0 +1,19 @@
+print "Please enter a verb:" + " "
+verb = gets
+
+print "Please enter a noun:" + " "
+noun = gets
+
+print "Please enter a adjective:" + " "
+adjective = gets
+
+print "Please enter a second noun:" + " "
+noun2 = gets
+
+puts "One day, I decided to learn to #{verb.chomp} in Ruby."
+
+puts "So I turned on my #{noun.chomp} and logged in to Treehouse."
+
+puts "Their teachers were really #{adjective.chomp}."
+
+puts "In no time, I'd learned to program a simple #{noun2.chomp}!"
